5e6dd84907f0ac05ddc0122419a3120622966a4c
Davi Luiz Guides <davi.guides@gmail.com>
2025-04-18 16:57:44 -0300

    Fix minor style issues in rabbitmqadmin formula based on brew style recommendations

--- Formula/rabbitmqadmin.rb
+++ Formula/rabbitmqadmin.rb
@@ -3,7 +3,7 @@ class Rabbitmqadmin < Formula
   homepage "https://github.com/rabbitmq/rabbitmqadmin-ng"
   url "https://github.com/rabbitmq/rabbitmqadmin-ng/releases/download/v2.0.0/rabbitmqadmin-2.0.0-aarch64-apple-darwin"
   version "2.0.0"
-  sha256 "a211d82b3b6780c488f3838a49b9f4d02684afa1010c44fa98fcd5b8cd844ab7"  # calculated below
+  sha256 "a211d82b3b6780c488f3838a49b9f4d02684afa1010c44fa98fcd5b8cd844ab7"
 
   def install
     bin.install "rabbitmqadmin-2.0.0-aarch64-apple-darwin" => "rabbitmqadmin"
